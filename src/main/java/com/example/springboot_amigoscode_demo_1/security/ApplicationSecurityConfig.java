4e942c3a286cc2634239ffd75c822ac3cca1cae8
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-06-29 22:15:49 +0530

    roles added

--- src/main/java/com/example/springboot_amigoscode_demo_1/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springboot_amigoscode_demo_1/security/ApplicationSecurityConfig.java
@@ -1,25 +1,34 @@
-package com.example.springboot_amigoscode_demo_1;
+package com.example.springboot_amigoscode_demo_1.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -31,12 +40,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails indialone = User.builder()
                 .username("indialone")
-                .password("HardySis@123")
-                .roles("STUDENT")
+                .password(passwordEncoder.encode("HardySis@123"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails indialoneAdmin = User.builder()
+                .username("indialoneadmin")
+                .password(passwordEncoder.encode("uru@123"))
+                .roles(ApplicationUserRole.ADMIN.name())
                 .build();
 
         return new InMemoryUserDetailsManager(
-                indialone
+                indialone,
+                indialoneAdmin
         );
     }
 }
---
+++ src/main/java/com/example/springboot_amigoscode_demo_1/security/ApplicationUserRole.java
@@ -0,0 +1,23 @@
+package com.example.springboot_amigoscode_demo_1.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.example.springboot_amigoscode_demo_1.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<ApplicationUserPermission> getPermissions() {
+        return permissions;
+    }
+}
